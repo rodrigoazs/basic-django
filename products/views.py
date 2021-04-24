a82344e7148cc2f158268629e1612b1fa1a599c7
Rodrigo Azevedo <rodrigo4zevedo@gmail.com>
2021-04-24 16:45:21 -0300

    Added routes for the api

--- products/views.py
+++ products/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
 
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
+ 
+from products.models import Product
+from products.serializers import ProductSerializer
+from rest_framework.decorators import api_view
+
 # Create your views here.
+@api_view(['GET', 'POST'])
+def product_list(request):
+    # get list of products
+    if request.method == 'GET':
+        products = Product.objects.all()
+        
+        title = request.GET.get('title', None)
+        if title is not None:
+            products = products.filter(title__icontains=title)
+        
+        products_serializer = ProductSerializer(products, many=True)
+        return JsonResponse(products_serializer.data, safe=False)
+        # 'safe=False' for objects serialization
+    # insert a product
+    elif request.method == 'POST':
+        product_data = JSONParser().parse(request)
+        product_serializer = ProductSerializer(data=product_data)
+        if product_serializer.is_valid():
+            product_serializer.save()
+            return JsonResponse(product_serializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    # find product by pk (id)
+    try: 
+        product = Product.objects.get(pk=pk) 
+    except Product.DoesNotExist: 
+        return JsonResponse({'message': 'The product does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+ 
+    # GET / PUT / DELETE tutorial product
+    # show product detail
+    if request.method == 'GET': 
+        product_serializer = ProductSerializer(product) 
+        return JsonResponse(product_serializer.data)
+    # update product detail
+    elif request.method == 'PUT': 
+        product_data = JSONParser().parse(request) 
+        product_serializer = ProductSerializer(product, data=product_data) 
+        if product_serializer.is_valid(): 
+            product_serializer.save() 
+            return JsonResponse(product_serializer.data) 
+        return JsonResponse(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # delete product
+    elif request.method == 'DELETE': 
+        product.delete()
+        return JsonResponse({'message': 'The product was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
